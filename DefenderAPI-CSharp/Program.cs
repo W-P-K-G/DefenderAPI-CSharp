5cf202f8cc45ab4a83852e935e116f3138dc2454
Ploch <Ploch@LENOVO>
2022-07-23 18:36:39 +0200

    Dodaj pliki projektów.

---
+++ DefenderAPI-CSharp/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DefenderAPI_CSharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DefenderAPI defenderAPI = new DefenderAPI();
+            defenderAPI.addExclusionFolder(@"C:\Users\Ploch\Downloads");
+        }
+    }
+}
